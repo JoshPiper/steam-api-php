2d263752943eeb6233bce2de68b8f6223752d388
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-11-28 13:13:23 +0000

    Add the abstract Command class.
    This means we can add default functionality without changing every command.

---
+++ src/Steam/Command/Command.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Steam\Command;
+
+
+abstract class Command implements CommandInterface {
+	const REQUESTKEY_DEFAULT = null;
+	const REQUESTKEY_QUERY = 'query';
+	const REQUESTKEY_BODY = 'body';
+	/**
+	 * @inheritDoc
+	 */
+	public function getRequestKey(){
+		return static::REQUESTKEY_DEFAULT;
+	}
+}
