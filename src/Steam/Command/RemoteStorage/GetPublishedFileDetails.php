278fc2e4109f31d481186ee68326c90185543a7f
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-11-28 11:10:09 +0000

    Add GetPublishedFileDetails.

---
+++ src/Steam/Command/RemoteStorage/GetPublishedFileDetails.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Steam\Command\RemoteStorage;
+
+
+class GetPublishedFileDetails extends RemoteStorage {
+	protected $fileIds = [];
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getMethod(){
+		return 'GetCollectionDetails';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRequestMethod(){
+		'POST';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getParams(){
+		$params = [
+			'itemcount' => count($this->fileIds),
+		];
+		foreach ($this->fileIds as $idx => $file_id){
+			$params["publishedfileids[{$idx}]"] = $file_id;
+		}
+		return $params;
+	}
+}
